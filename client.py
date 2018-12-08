aa3a12c070ee016c67e93d22b03d3a4093353ee4
Ashley Teoh <ashleytqy@gmail.com>
2018-12-08 14:36:11 -0500

    some questions...

--- client.py
+++ client.py
@@ -9,6 +9,8 @@ class Client:
     self.folder = '/tmp/' + port_number
     self.file_splitter = FileSplitter()
 
+    # shoud we have only 1 socket for this client, or 1 socket per peer?
+
     # list of peers it is connected to
     # peers need to know what pieces of the content each peer in its peer set has
     self.peer_set = []
@@ -23,16 +25,26 @@ class Client:
 
   def notify_tracker(self, port = 8000):
     tracker_address = ("127.0.0.1", 8000)
-    print(tracker_address)
     sock = socket(AF_INET, SOCK_STREAM)
     sock.connect(tracker_address)
     message = 'hello from client!'
     sock.send(message.encode("utf-8"))
     print("sent: " + message + "\n")
+    # if recieved an ack, return True, else False / throw an error?
+    # leave it to init.py to catch the error
     # sock.close()
 
   def connect(self, peer):
     # first establish the connection
+    # peer ==> port
+    peer_address = ("127.0.0.1", peer)
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.connect(peer_address)
+    message = 'hello from client!'
+    sock.send(message.encode("utf-8"))
+    print("sent: " + message + "\n")
+
+    # if connection is successful, add peer to peer set
     self.peer_set.append(peer)
 
   def upload(self, file_location):
@@ -40,7 +52,6 @@ class Client:
     # split file into equal parts
     # get all active peers
     # distribute file parts to specific peers
-    
 
   def download(self, file_id):
     # first, talk to tracker and get the info
