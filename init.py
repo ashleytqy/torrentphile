bc5976c278c56dd86bc5f9c4e38801359230a021
Ashley Teoh <ashleytqy@gmail.com>
2018-12-08 13:56:32 -0500

    file path as an args

--- init.py
+++ init.py
@@ -6,12 +6,13 @@ def run():
   parser = argparse.ArgumentParser(description='stuff.')
   parser.add_argument('--type', help='client or tracker', choices=['client', 'tracker'])
   parser.add_argument('--port', help='port to run on')
+  parser.add_argument('--file', help='path of file to upload')
 
   args = parser.parse_args()
 
   if args.type == 'client':
     client = Client(args.port)
-    client.upload('/tmp/test.txt')
+    client.upload(args.file)
   else:
     tracker = Tracker()
 
