7b65a7be142268e6d90fd6ce645fd22ff96464da
Ashley Teoh <ashleytqy@gmail.com>
2018-12-08 13:52:50 -0500

    done: split extension and name

--- file_splitter.py
+++ file_splitter.py
@@ -1,26 +1,25 @@
 # coding: utf-8
-
-import sys
 import os
-from socket import *
 
 # clean up: https://superuser.com/questions/482435/how-to-remove-all-files-starting-with-a-certain-string-in-linux/482436
 # from https://www.tutorialspoint.com/How-to-spilt-a-binary-file-into-multiple-files-using-Python
+
 class FileSplitter:
-  def __init__(self, chunk_size = 50):
+  def __init__(self, chunk_size = 500):
     self.chunk_size = chunk_size
 
-  def split(self, file):
+  def split(self, file_path):
     chunk_file_names = []
+    file_name, file_ext = os.path.splitext(file_path)
     
     file_number = 1
-    with open(file) as f:
-        chunk = f.read(self.chunk_size)
+    with open(file_path) as file:
+        chunk = file.read(self.chunk_size)
         while chunk:
-            with open(file + str(file_number), "w+") as chunk_file:
+            with open(file_name + str(file_number) + file_ext, "w+") as chunk_file:
                 chunk_file.write(chunk)
             file_number += 1
-            chunk = f.read(self.chunk_size)
+            chunk = file.read(self.chunk_size)
             chunk_file_names.append(chunk_file.name)
 
     return chunk_file_names
